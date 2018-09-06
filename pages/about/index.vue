d158a8a0ae2fad6449e8960b78f2009928d3ba95
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-09-06 16:38:03 +1000

    file fix

---
+++ pages/about/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1> The About Page </h1>
+</template>
+
+<style scoped>
+h1 {
+    text-align: center;
+}
---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <section class="single-recipe">
+        <h1> {{ recipe.title }} </h1>
+        <div> 
+            <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.title">
+        </div>
+        <p> The recipe description. </p>
+    </section>
+</template>
+
+<script>
+export default {
+    asyncData(context) {
+      return new Promise((resolve, request) => {
+          setTimeout(() => {
+              resolve({
+                  recipe: [
+                    {
+                      id: "1",
+                      title: "Delicious Pizza",
+                      previewText: "Awesome Pizza",
+                      descriptionText: "This is really delicious",
+                      thumbnail: "https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/117944.jpg",
+                    },
+                    {
+                      id: "2",
+                      title: "Sea Foood",
+                      previewText: "Sushi",
+                      descriptionText: "This is really delicious",
+                      thumbnail: "https://www.ennri.com/images/slides/slide_05.png",
+                    }
+                ].find(el => el.id === context.params.id)
+              })
+          }, 1500 )
+      })
+    }
+}
+</script>
+
+<style scoped>
+.single-recipe {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 30px;
+}
+
+.recipe-image {
+    width: 100%;
+}
+</style>
